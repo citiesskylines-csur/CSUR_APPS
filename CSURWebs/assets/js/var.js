beec6438d565852280c58693b15cc9d9a0810136
PhantomCTS <39853670+PhantomCTS@users.noreply.github.com>
2021-05-24 12:58:33 +0800

    Add files via upload

---
+++ CSURWebs/assets/js/var.js
@@ -0,0 +1,32 @@
+var roadcode;
+function var_roadname() {
+	roadcode = document.getElementById('roadcode').value;
+	if (document.getElementById('CNMan1').checked == true){
+		roadcode += " -r";
+	}
+	if (document.getElementById('CNMan2').checked == true){
+		roadcode += " _express";
+	}
+	if (document.getElementById('CNMan3').checked == true){
+		roadcode += " _compact";
+	}
+	//Formium.external.Roadinfo.Roadcode = document.getElementById('roadcode').value;
+	if (roadcode != null) {
+		Formium.external.Roadinfo.Roadcode = roadcode;
+		Lobibox.notify('info', {
+			pauseDelayOnHover: true,
+			continueDelayOnInactiveTab: false,
+			position: 'top right',
+			icon: 'bx bx-info-circle',
+			msg: 'Generating'
+		});
+	} else {
+		Lobibox.notify('warning', {
+			pauseDelayOnHover: true,
+			continueDelayOnInactiveTab: false,
+			position: 'top right',
+			icon: 'bx bx-info-circle',
+			msg: 'Please enter your modules name'
+		});
+	}
+}
